d6547c0beebdcef6cb487c67ba3986ce8c6ef598
Anghelo Carvajal <angheloalf95@gmail.com>
2026-03-09 22:31:08 -0300

    Compiles

--- src/addended_ordered_map/src/python_bindings/py_addended_ordered_map.rs
+++ src/addended_ordered_map/src/python_bindings/py_addended_ordered_map.rs
@@ -1,13 +1,16 @@
 /* SPDX-FileCopyrightText: © 2026 Decompollaborate */
 /* SPDX-License-Identifier: MIT OR Apache-2.0 */
 
+use pyo3::exceptions::PyRuntimeError;
 use pyo3::prelude::*;
+use pyo3::types::PyInt;
 
 use crate::{AddendedOrderedMap, FindSettings};
+use crate::python_bindings::PySizedValueBase;
 
 #[pyclass(name = "AddendedOrderedMap")]
-struct PyAddendedOrderedMap {
-    inner: AddendedOrderedMap<u64, Py<PyAny>, Option<u64>>,
+pub struct PyAddendedOrderedMap {
+    inner: AddendedOrderedMap<u64, Py<PySizedValueBase>, u64>,
 }
 
 #[pymethods]
@@ -19,11 +22,18 @@ impl PyAddendedOrderedMap {
         }
     }
 
-    /*
-    pub fn find<'py>(&self, key: &Bound<'py, PyAny>/*, settings: FindSettings */) -> PyResult<Option<(Py<PyAny>, &Py<PyAny>)>> {
-        let extracted_key = key.extract()?;
-        self.inner.find(extracted_key, FindSettings::new(true));
-        todo!()
+    pub fn find<'py>(&self, key: &Bound<'py, PyAny>/*, settings: FindSettings */) -> PyResult<Option<(Py<PyInt>, &Py<PySizedValueBase>)>> {
+        let extracted_key: u64 = key.extract()?;
+
+        if let Some((k, v)) = self.inner.find(&extracted_key, FindSettings::new(true)) {
+            let k2 = Python::try_attach(|py| {
+                k.into_pyobject(py).map(|x| x.unbind())
+            }).ok_or_else(|| PyRuntimeError::new_err("Internal conversion error"))??;
+
+            Ok(Some((k2, v)))
+        } else {
+            Ok(None)
+        }
     }
-    */
 }
+
--- src/addended_ordered_map/src/python_bindings/py_sized_value.rs
+++ src/addended_ordered_map/src/python_bindings/py_sized_value.rs
@@ -2,17 +2,27 @@
 /* SPDX-License-Identifier: MIT OR Apache-2.0 */
 
 use pyo3::prelude::*;
+use pyo3::exceptions::PyNotImplementedError;
 
-use crate::{SizedValue};
+use crate::SizedValue;
 
-#[pyclass(name = "SizedValue", subclass)]
-struct PySizedValueBase {}
+#[pyclass(name = "SizedValue", subclass, extends=pyo3::types::PyAny)]
+#[non_exhaustive]
+pub struct PySizedValueBase {}
 
 impl PySizedValueBase {
-    fn get_size_from_py_subclass<T>(&self) -> T {
-        
-        todo!()
+    pub fn get_size(&self) -> PyResult<u64> {
+        Err(PyNotImplementedError::new_err(
+            "get_size must be implemented by subclass",
+        ))
     }
 }
 
-
+impl SizedValue<u64> for Py<PySizedValueBase> {
+    fn size(&self) -> u64 {
+        Python::try_attach(|py| {
+            let size = self.call_method0(py, "get_size").unwrap();
+            size.extract(py).unwrap()
+        }).unwrap()
+    }
+}
