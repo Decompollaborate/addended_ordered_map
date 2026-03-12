960165d071e27522cfdddba4744e86cef681a1ad
Anghelo Carvajal <angheloalf95@gmail.com>
2026-03-12 08:35:38 -0300

    Manage to package the type hints

--- src/addended_ordered_map/src/python_bindings/py_sized_value.rs
+++ src/addended_ordered_map/src/python_bindings/py_sized_value.rs
@@ -3,18 +3,23 @@
 
 use pyo3::prelude::*;
 use pyo3::exceptions::PyNotImplementedError;
+use pyo3::types::{PyDict, PyTuple};
 
 use crate::SizedValue;
 
 #[pyclass(name = "SizedValue", module="addended_ordered_map", subclass, extends=pyo3::types::PyAny)]
 #[non_exhaustive]
-pub struct PySizedValueBase {}
+pub struct PySizedValueBase;
 
 #[pymethods]
 impl PySizedValueBase {
     #[new]
-    pub fn new() -> Self {
-        Self {}
+    #[pyo3(signature = (*args, **kwargs))]
+    pub fn __new__(
+        #[expect(unused_variables)] args: &Bound<'_, PyTuple>,
+        #[expect(unused_variables)] kwargs: Option<&Bound<'_, PyDict>>,
+    ) -> Self {
+        Self
     }
 
     pub fn get_size(&self) -> PyResult<u64> {
--- tests/python/basic_test.py
+++ tests/python/basic_test.py
@@ -7,31 +7,37 @@ import addended_ordered_map
 
 
 class TestValue(addended_ordered_map.SizedValue):
-    def __init__(self, size: int, whatever: int = 5) -> None:
+    def __init__(self, key: int, size: int) -> None:
+        self.key = key
         self.size = size
 
     def get_size(self) -> int:
         return self.size
 
+    def __eq__(self, other) -> bool:
+        if isinstance(other, TestValue):
+            return self.key == other.key and self.size == other.size
+        return False
+
 
 def basic_test():
     test_map = addended_ordered_map.AddendedOrderedMap()
 
-    test_map.find_or_insert(0x100C, TestValue(1))
-    test_map.find_or_insert(0x1000, TestValue(4))
-    test_map.find_or_insert(0x1004, TestValue(4))
+    test_map.find_or_insert(0x100C, TestValue(0x100C, 1))
+    test_map.find_or_insert(0x1000, TestValue(0x1000, 4))
+    test_map.find_or_insert(0x1004, TestValue(0x1004, 4))
 
-    assert (0x1000, TestValue(4)) == test_map.find(0x1000)
+    assert test_map.find(0x1000) == (0x1000, TestValue(0x1000, 4))
 
-    assert (0x1000, TestValue(4)) == test_map.find(0x1002)
+    assert test_map.find(0x1002) == (0x1000, TestValue(0x1000, 4))
 
-    assert None == test_map.find(0x0F00)
+    assert test_map.find(0x0F00) is None
 
-    assert None == test_map.find(0x2000)
+    assert test_map.find(0x2000) is None
 
-    # assert None == test_map.find(0x1002)
+    # assert test_map.find(0x1002) is None
 
-    assert None == test_map.find(0x1008)
+    assert test_map.find(0x1008) is None
 
 
 basic_test()
